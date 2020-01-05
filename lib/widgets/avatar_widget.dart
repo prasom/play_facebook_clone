8e5f9ac93c98ff9956a1052ad0aaa3f79f2213a7
Pornsak Prasom <pornsak.prasom@allianz.com>
2020-01-05 17:54:52 +0700

    feat:add facebook clone

---
+++ lib/widgets/avatar_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Avatar extends StatefulWidget {
+  Avatar({Key key}) : super(key: key);
+
+  @override
+  _AvatarState createState() => _AvatarState();
+}
+
+class _AvatarState extends State<Avatar> {
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      child: FlutterLogo(),
+    );
+  }
+}
